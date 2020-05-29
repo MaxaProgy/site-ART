f686217c9757db7a44c3cbd04c2ba5dbd49b5d25
Zyryanova Maria <mariazyranova72@gmail.com>
2020-05-29 19:33:13 +0500

    блок автора на странице поиска по авторам

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import logging
 
 from data import db_session
 from data.article import Articles
+from data.users import User
+from form.loginform import LoginForm
+from flask_login import login_user
 from data.artist import Artist
 
 logging.basicConfig(level=logging.INFO)
@@ -16,19 +19,50 @@ app.config['SECRET_KEY'] = os.urandom(24)
 def index():
     session = db_session.create_session()
     articles = session.query(Articles).all()
-    article_random = random.sample(articles, len(articles))[0]
-    article_past = session.query(Articles).filter(Articles.create_date).first()
+    article_random = None
+    article_past = None
+    if articles:
+        article_random = random.sample(articles, len(articles))[0]
+        article_past = session.query(Articles).filter(Articles.create_date).first()
     return render_template('main.html', title='Art.',
                            article_random=article_random, article_past=article_past)
+
+
+@app.route('/admin', methods=['GET', "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.login == form.login.data).first()
+
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/search/artist', methods=['GET'])
 def search_artist():
     session = db_session.create_session()
-    artists = session.query(Artist).all()
+    q = request.args.get('q')
+    if q:
+        artists = session.query(Artist).filter(Artist.name.like(f'%{q}%') | Artist.text.like(f'%{q}%')).all()
+    else:
+        artists = session.query(Artist).all()
     return render_template('search_artist.html', title='Художники', artists=artists)
 
 
 db_session.global_init("db/art_point_db.sqlite")
+session = db_session.create_session()
+user = session.query(User).first()
+if user is None:
+    user = User(login="admin", name="admin", email="admin@admin.ru", hashed_password="1111")
+    user.set_password("1111")
+    session.add(user)
+    session.commit()
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
