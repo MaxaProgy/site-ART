06dc59048a01320da9fadac303c7f393db8b6699
AlexZyryanov72 <juhome1980@gmail.com>
2020-05-29 14:54:51 +0500

    Разработка админки

---
+++ form/loginform.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    login = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
